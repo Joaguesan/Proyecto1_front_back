36bbed424fcb4570eaef8213ca1b562900ab63a5
a22biepalgon <a22biepalgon@inspedralbes.cat>
2023-10-23 20:46:01 +0200

    BuscarId funciona correctament, sense controlerrors

--- tenda/src/components/GestioComandes.vue
+++ tenda/src/components/GestioComandes.vue
@@ -22,16 +22,16 @@
 
                 <v-row>
                     <v-col cols="3"><v-select v-model="seleccio" density="compact"
-                            :items="['Pendents', 'Acceptades', 'Tancades']"></v-select></v-col>
+                            :items="['Pendents', 'Acceptades', 'Tancades']" @update:menu=buscarComandes(seleccio) ></v-select></v-col>
                     <v-col cols="6"></v-col>
                     <v-col cols="3"><v-btn @click="dialog = true" class="">Cercar Comandes</v-btn>
                         <v-dialog v-model="dialog" width="auto">
                             <v-card>
                                 <v-card-text>ID a buscar: </v-card-text>
                                 <v-text-field v-model="idcomanda" :counter="10" label="ID comanda"
-                                    required hide-details @key.up.enter="buscar"></v-text-field>
+                                    required hide-details @key.up.enter=""></v-text-field>
                                 <v-card-actions>
-                                    <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
+                                    <v-btn color="primary" block @click="buscarComanda(idcomanda); dialog = false">Close Dialog</v-btn>
                                 </v-card-actions>
                             </v-card>
                         </v-dialog>
@@ -50,7 +50,7 @@
                         </v-row>
                         <v-row :class="[
                             index % 2 === 0 ? 'bg-grey-lighten-2' : 'bg-white'
-                        ]" v-for="(comanda, index) in comandes.filter(comanda => comanda.estat === seleccio)"
+                        ]" v-for="(comanda, index) in comandes"
                             :key=index>
                             <v-col cols="2">{{ comanda.idComanda }}</v-col>
                             <v-col cols="2">{{ comanda.idClient }}</v-col>
@@ -82,88 +82,98 @@ export default {
         ultimaColumna: "Hola",
         dialog: false,
         idcomanda:"",
-        comandes: [
-            {
-                "idComanda": 1,
-                "idClient": 101,
-                "data": "2023-10-23",
-                "import": 50.99,
-                "estat": "Pendents"
-            },
-            {
-                "idComanda": 2,
-                "idClient": 102,
-                "data": "2023-10-24",
-                "import": 75.50,
-                "estat": "Pendents"
-            },
-            {
-                "idComanda": 3,
-                "idClient": 103,
-                "data": "2023-10-25",
-                "import": 30.25,
-                "estat": "Acceptades"
-            },
-            {
-                "idComanda": 4,
-                "idClient": 104,
-                "data": "2023-10-26",
-                "import": 45.75,
-                "estat": "Acceptades"
-            },
-            {
-                "idComanda": 5,
-                "idClient": 105,
-                "data": "2023-10-27",
-                "import": 60.00,
-                "estat": "Acceptades"
-            },
-            {
-                "idComanda": 6,
-                "idClient": 106,
-                "data": "2023-10-28",
-                "import": 90.99,
-                "estat": "Tancades"
-            },
-            {
-                "idComanda": 7,
-                "idClient": 107,
-                "data": "2023-10-29",
-                "import": 25.49,
-                "estat": "Tancades"
-            },
-            {
-                "idComanda": 8,
-                "idClient": 108,
-                "data": "2023-10-30",
-                "import": 55.75,
-                "estat": "Tancades"
-            },
-            {
-                "idComanda": 9,
-                "idClient": 109,
-                "data": "2023-10-31",
-                "import": 40.00,
-                "estat": "Tancades"
-            },
-            {
-                "idComanda": 10,
-                "idClient": 110,
-                "data": "2023-11-01",
-                "import": 70.25,
-                "estat": "Pendents"
-            }
-        ]
+        comandes:  [],
+        comandesGlobal: [
+    {
+        "idComanda": 1,
+        "idClient": 101,
+        "data": "2023-10-23",
+        "import": 50.99,
+        "estat": "Pendents"
+    },
+    {
+        "idComanda": 2,
+        "idClient": 102,
+        "data": "2023-10-24",
+        "import": 75.50,
+        "estat": "Pendents"
+    },
+    {
+        "idComanda": 3,
+        "idClient": 103,
+        "data": "2023-10-25",
+        "import": 30.25,
+        "estat": "Acceptades"
+    },
+    {
+        "idComanda": 4,
+        "idClient": 104,
+        "data": "2023-10-26",
+        "import": 45.75,
+        "estat": "Acceptades"
+    },
+    {
+        "idComanda": 5,
+        "idClient": 105,
+        "data": "2023-10-27",
+        "import": 60.00,
+        "estat": "Acceptades"
+    },
+    {
+        "idComanda": 6,
+        "idClient": 106,
+        "data": "2023-10-28",
+        "import": 90.99,
+        "estat": "Tancades"
+    },
+    {
+        "idComanda": 7,
+        "idClient": 107,
+        "data": "2023-10-29",
+        "import": 25.49,
+        "estat": "Tancades"
+    },
+    {
+        "idComanda": 8,
+        "idClient": 108,
+        "data": "2023-10-30",
+        "import": 55.75,
+        "estat": "Tancades"
+    },
+    {
+        "idComanda": 9,
+        "idClient": 109,
+        "data": "2023-10-31",
+        "import": 40.00,
+        "estat": "Tancades"
+    },
+    {
+        "idComanda": 10,
+        "idClient": 110,
+        "data": "2023-11-01",
+        "import": 70.25,
+        "estat": "Pendents"
+    }
+]
 
     }),
+    created() {
+        this.buscarComandes("Pendents");
+    },
     methods: {
         cambio() {
             this.nombre = this.nombre === 'Gestio Productes' ? 'Gestio Comandas' : 'Gestio Productes'
             this.$router.push(this.link)
             this.link = this.link === 'gestiocomandes' ? 'gestioproductes' : 'gestiocomandes'
         },
-        buscar(){
-
+        buscarComandes(estat){
+            console.log(estat)
+            this.comandes = this.comandesGlobal.filter(comanda => comanda.estat === estat);
+            console.log(this.comandes)
+        },
+        buscarComanda(id){
+            this.comandes = this.comandesGlobal.filter(comanda => comanda.idComanda == id);
+            console.log(this.comandes)
         }
     },
     computed: {
