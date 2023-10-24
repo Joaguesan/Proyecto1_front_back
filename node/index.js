b52b081a2c82664cbea5d0f06cdb786b0c456815
a21ismalgelo <a21ismalgelo@inspedralbes.cat>
2023-10-24 09:32:39 +0200

    API CRUD routes completely created and operational

--- node/index.js
+++ node/index.js
@@ -8,78 +8,70 @@ const port = 3000;
 
 const mysql = require("mysql2");
 
-var conn = mysql.createConnection({
+var conn = mysql.createPool({
   host: "dam.inspedralbes.cat",
   user: "a22joaguesan_num3",
   password: "G3proyecto1",
   database: "a22joaguesan_tienda",
+  connectionLimit: 10,
+  queueLimit: 0,
+  waitForConnections: true,
 });
 
-// conn.connect((err) => {
-//   if (err) console.error(err);
-//   console.log("Connected to database!");
-// });
-
-// conn.end((err) => {
-//   if (err) console.error(err);
-//   console.log("Disconnected from database!");
-// });
+conn.getConnection((err, connection) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Connected to database!");
+  }
+});
 
 app.get("/getProducts", async (req, res) => {
-  // try {
-    await new Promise((resolve, reject) => {
-      conn.connect((err) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
-
     var sql = "SELECT * FROM Producto";
 
     conn.query(sql, (err, result) => {
       if (err) console.error(err);
       res.send(result);
-
-      conn.end((err) => {
-        if (err) console.error(err);
-      });
     });
-  // } catch (err) {
-  //   res.status(500).send("Error connecting the database.");
-  // }
+});
+
+app.get("/getOneProduct/:id", async (req, res) => {
+  var sql = `SELECT * FROM Producto WHERE IDProducto = ${req.params.id}`;
+
+  conn.query(sql, (err, result) => {
+    if (err) console.error(err);
+    res.send(result);
+  });
 });
 
 app.post("/addProduct", async (req, res) => {
-  // try {
-    await new Promise((resolve, reject) => {
-      conn.connect((err) => {
-        if(err){
-          console.error(err);
-          reject(err);
-        }else{
-          resolve();
-        }
-      });
-    });
-
     var sql = `INSERT INTO Producto (NombreProducto,Descripcion,PrecioUnitario) VALUES ('${req.body.name}','${req.body.description}','${req.body.price}')`;
 
     conn.query(sql, (err, result) => {
       if (err) console.error(err);
       console.log(result);
       res.send(result);
-
-      conn.end((err) => {
-        if (err) console.error(err);
-      });
     });
-  // } catch {
-  //   res.status(500).send("Error connecting the database.");
-  // }
+});
+
+app.delete("/deleteProduct", async (req, res) => {
+    var sql = `DELETE FROM Producto WHERE IDProducto = ${req.body.id}`;
+
+    conn.query(sql, (err, result) => {
+      if (err) console.error(err);
+      console.log(result);
+      res.send(result);
+    });
+});
+
+app.put("/updateProduct/:id", async (req, res) => {
+  var sql = `UPDATE Producto SET NombreProducto = '${req.body.name}', Descripcion = '${req.body.description}', PrecioUnitario = '${req.body.price}' WHERE IDProducto = ${req.params.id}`
+
+  conn.query(sql, (err, result) => {
+    if (err) console.error(err);
+    console.log(result);
+    res.send(result);
+  });
 });
 
 app.listen(port, () => {
