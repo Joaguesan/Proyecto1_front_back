18de3f37f323360e55a85985ab6344c3d010aa93
a21marsanbla <a21marsanbla@inspedralbes.cat>
2023-10-25 10:57:35 +0200

    estado pedido

--- tenda/src/components/gestioProductos.vue
+++ tenda/src/components/gestioProductos.vue
@@ -5,7 +5,7 @@
                 <v-btn variant="plain" @click="cambio">
                     {{ nombre }}
                 </v-btn>
-                <v-btn icon="	mdi-chevron-down"></v-btn>
+                <v-btn icon="mdi-cached" @click="recargar()"></v-btn>
             </template>
         </v-app-bar>
 
@@ -28,14 +28,14 @@
                                     <v-card-item>
                                         <v-img :src="productoNuevo.Imatge"></v-img>
                                         <v-container>
-                                            <v-text-field v-model="productoNuevo.NombreProducto" :rules="[Regla.required]"  label="Nombre del producto"
-                                                variante="outlined" required></v-text-field>
-                                            <v-text-field v-model="productoNuevo.PrecioUnitario" suffix="€" label="Precio"
+                                            <v-text-field v-model="productoNuevo.name" :rules="[Regla.required]"
+                                                label="Nombre del producto" variante="outlined" required></v-text-field>
+                                            <v-text-field v-model="productoNuevo.price" suffix="€" label="Precio"
                                                 :rules="[Regla.required]" required></v-text-field>
-                                            <v-textarea v-model="productoNuevo.Descripcion" :rules="[Regla.required]" label="Descripcion"
-                                                auto-grow required></v-textarea>
-                                            <v-text-field v-model="productoNuevo.Imatge" :rules="[Regla.required]" label="URL imagen"
-                                                variante="outlined" required></v-text-field>
+                                            <v-textarea v-model="productoNuevo.description" :rules="[Regla.required]"
+                                                label="Descripcion" auto-grow required></v-textarea>
+                                            <v-text-field v-model="productoNuevo.Imatge" :rules="[Regla.required]"
+                                                label="URL imagen" variante="outlined" required></v-text-field>
                                         </v-container>
                                     </v-card-item>
                                     <v-card-actions>
@@ -47,6 +47,7 @@
                         </v-dialog>
                     </v-col>
                 </v-row>
+                <!--Habilitados-->
                 <v-container>
                     <v-row>
                         <v-col v-for="(variant, i) in productos1" :key="i" cols="3">
@@ -64,18 +65,19 @@
                                     <v-btn color="primary" @click="Deshabilitar(i)">Deshabilitar</v-btn>
                                 </v-card-actions>
                             </v-card>
+                <!--Edicion Habilitados-->
                             <v-card v-else-if="variant.reveal & variant.Habilitado" class="mx-auto" max-width="344">
                                 <v-container>
                                     <v-card-item>
-                                        <v-img :src="productoViejo.Imatge"></v-img>
+                                        <v-img :src="productoNuevo.Imatge"></v-img>
                                         <v-container>
-                                            <v-text-field v-model="productoViejo.NombreProducto" :rules="[Regla.required]"
+                                            <v-text-field v-model="productoNuevo.name" :rules="[Regla.required]"
                                                 variant="outlined" required></v-text-field>
-                                            <v-text-field v-model="productoViejo.PrecioUnitario" suffix="€"
+                                            <v-text-field v-model="productoNuevo.price" suffix="€"
                                                 :rules="[Regla.required]" required></v-text-field>
-                                            <v-textarea v-model="productoViejo.Descripcion" :rules="[Regla.required]"
+                                            <v-textarea v-model="productoNuevo.description" :rules="[Regla.required]"
                                                 auto-grow required></v-textarea>
-                                            <v-text-field v-model="productoViejo.Imatge" :rules="[Regla.required]"
+                                            <v-text-field v-model="productoNuevo.Imatge" :rules="[Regla.required]"
                                                 variant="outlined" required></v-text-field>
                                         </v-container>
                                     </v-card-item>
@@ -89,6 +91,8 @@
                     </v-row>
                 </v-container>
                 <v-divider :thickness="5" class="border-opacity-70"></v-divider>
+                
+                <!--Deshabilitados-->
                 <v-container>
                     <v-row justify="left">
                         <v-col v-for="(variante, i) in productos1" :key="i" cols="auto">
@@ -108,9 +112,10 @@
                                     <v-dialog v-model="dialog" width="auto">
                                         <v-card>
                                             <v-card-title class="text-h5">
-                                               Eliminar {{ variante.NombreProducto }} ?
+                                                Eliminar {{ variante.NombreProducto }} ?
                                             </v-card-title>
-                                            <v-card-text>¿Estas seguro que quieres eliminar {{ variante.NombreProducto }}?</v-card-text>
+                                            <v-card-text>¿Estas seguro que quieres eliminar {{ variante.NombreProducto
+                                            }}?</v-card-text>
                                             <v-card-actions>
                                                 <v-btn color="primary" @click="dialog = false">Aceptar</v-btn>
                                                 <v-btn color="primary" @click="dialog = false">Cancelar</v-btn>
@@ -119,18 +124,20 @@
                                     </v-dialog>
                                 </v-card-actions>
                             </v-card>
+                            
+                <!--edicion deshabilitados-->
                             <v-card v-else-if="variante.reveal & !variante.Habilitado" class="mx-auto" max-width="344">
                                 <v-container>
                                     <v-card-item>
-                                        <v-img :src="productoViejo.Imatge"></v-img>
+                                        <v-img :src="productoNuevo.Imatge"></v-img>
                                         <v-container>
-                                            <v-text-field v-model="productoViejo.NombreProducto" :rules="[Regla.required]"
+                                            <v-text-field v-model="productoNuevo.name" :rules="[Regla.required]"
                                                 variante="outlined" required></v-text-field>
-                                            <v-text-field v-model="productoViejo.PrecioUnitario" suffix="€"
+                                            <v-text-field v-model="productoNuevo.price" suffix="€"
                                                 :rules="[Regla.required]" required></v-text-field>
-                                            <v-textarea v-model="productoViejo.Descripcion" :rules="[Regla.required]"
+                                            <v-textarea v-model="productoNuevo.description" :rules="[Regla.required]"
                                                 auto-grow required></v-textarea>
-                                            <v-text-field v-model="productoViejo.Imatge" :rules="[Regla.required]"
+                                            <v-text-field v-model="productoNuevo.Imatge" :rules="[Regla.required]"
                                                 variante="outlined" required></v-text-field>
                                         </v-container>
                                     </v-card-item>
@@ -147,26 +154,21 @@
     </v-layout>
 </template>
 <script>
-import { getProductos } from '@/manager'
+import { getProductos, UpdateProductos, AddProductos,CambiarEstado } from '@/manager'
 export default {
     data: () => ({
         nombre: "Gestio Productes",
         link: "gestiocomandes",
         dialog: false,
         dialog1: false,
-        productoViejo: {
-            NombreProducto: "",
-            PrecioUnitario: "",
-            Descripcion: "",
-            Imatge: ""
-        },productoNuevo: {
-            id:"",
-            NombreProducto: "",
-            PrecioUnitario: "",
-            Descripcion: "",
-            Imatge: ""
+         productoNuevo: {
+            id: "",
+            name: "",
+            price: "",
+            description: "",
+            Imatge:""
         },
-        productos1:[],
+        productos1: [],
         Regla: {
             required: value => !!value || 'Es requereix'
         }
@@ -181,32 +183,60 @@ export default {
             this.dialog = this.dialog === false ? true : false
         },
         editar(producto) {
-            this.productoViejo.NombreProducto = producto.NombreProducto
-            this.productoViejo.PrecioUnitario = producto.PrecioUnitario
-            this.productoViejo.Descripcion = producto.Descripcion
-            this.productoViejo.Imatge = producto.Imatge
+            this.productoNuevo.id = producto.IDProducto
+            this.productoNuevo.name = producto.NombreProducto
+            this.productoNuevo.price = producto.PrecioUnitario
+            this.productoNuevo.description = producto.Descripcion
+            this.productoNuevo.Imatge = producto.Imatge
             producto.reveal = true
         },
         aceptar(variant) {
-            variant.NombreProducto = this.productoViejo.NombreProducto
-            variant.PrecioUnitario = this.productoViejo.PrecioUnitario
-            variant.Descripcion = this.productoViejo.Descripcion
-            variant.Imatge = this.productoViejo.Imatge
-            variant.reveal = false
+            UpdateProductos(this.productoNuevo, this.productoNuevo.id).then((response) => {
+                variant.reveal = false
+                this.productoNuevo.id = ""
+                this.productoNuevo.name = ""
+                this.productoNuevo.price = ""
+                this.productoNuevo.description = ""
+            })
+            getProductos().then(response => this.productos1 = response)
+            
         },
         Deshabilitar(i) {
             this.productos1[i].Habilitado = this.productos1[i].Habilitado === false ? true : false
+            var habil = this.productos1[i].Habilitado
+            let estado={
+                "status": habil
+            }
+
+            CambiarEstado(estado, this.productos1[i].IDProducto).then((response) => {
+                this.productoNuevo.id = ""
+                this.productoNuevo.name = ""
+                this.productoNuevo.price = ""
+                this.productoNuevo.description = ""
+                estado.status=""
+            })
+            getProductos().then(response => this.productos1 = response)
+
         },
         eliminar() {
             dialog = true
         },
-        addProducto(){
-            this.productoNuevo.id = this.productos1.length+1
-            this.productos1.push(this.productoNuevo)
-            this.dialog1=false
+        addProducto() {
+            this.productoNuevo.id = this.productos1.length + 1
+            AddProductos(this.productoNuevo).then((response) => {
+                this.dialog1 = false
+                this.productoNuevo.id = ""
+                this.productoNuevo.name = ""
+                this.productoNuevo.price = ""
+                this.productoNuevo.description = ""
+                this.productos1  =getProductos()
+            })
+        },
+        recargar(){
+            getProductos().then(response => this.productos1 = response)
         }
-    },created() {
-        getProductos().then(response => this.productos1=response)
-  }
+    }, created() {
+        getProductos().then(response => this.productos1 = response)
+    }
 }
 </script>
--- tenda/src/manager.js
+++ tenda/src/manager.js
@@ -7,8 +7,31 @@ export async function getProductos() {
     const productos = await response.json();
     return productos;
 }
+export async function getUnProducto(id) {
+    const response = await fetch(`http://localhost:3000/getOneProduct/${id}`,
+        {
+            method: 'GET',
+            headers: { 'Accept': 'application/json' }
+        });
+    const producto = await response.json();
+    console.log(producto);
+    return producto;
+}
 export async function UpdateProductos(dadesProducte,id) {
-    const response = await fetch(`http://localhost:3000//updateProduct/${id}`,
+    console.log(dadesProducte);
+    const response = await fetch(`http://localhost:3000/updateProduct/${id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dadesProducte),
+            mode: "cors"
+        },);
+}
+export async function CambiarEstado(dadesProducte,id) {
+    console.log(dadesProducte);
+    const response = await fetch(`http://localhost:3000/productStatus/${id}`,
         {
             method: 'PUT',
             headers: {
