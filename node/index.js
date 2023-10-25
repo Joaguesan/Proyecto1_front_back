61aa005509c74a5744970a79dae6b08115b9571e
a21marsanbla <a21marsanbla@inspedralbes.cat>
2023-10-25 09:29:56 +0200

    adicion de cors

--- node/index.js
+++ node/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const cors = require("cors");
 // const cookieParser = require("cook0ie-parser");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const sessionsSecret = "2hCTcL2p5QMSny6DbZtUFjVtVXZqFa";
 const sessionConfig = {
@@ -61,7 +63,7 @@ app.get("/getOneProduct/:id", async (req, res) => {
 });
 
 app.post("/addProduct", async (req, res) => {
-  var sql = `INSERT INTO Producto (NombreProducto,Descripcion,PrecioUnitario) VALUES ('${req.body.name}','${req.body.description}','${req.body.price}')`;
+  var sql = `INSERT INTO Producto (NombreProducto,Descripcion,PrecioUnitario, Habilitado) VALUES ('${req.body.name}','${req.body.description}','${req.body.price}','${req.body.habilitado}')`;
 
   conn.query(sql, (err, result) => {
     if (err) console.error(err);
@@ -81,7 +83,7 @@ app.delete("/deleteProduct", async (req, res) => {
 });
 
 app.put("/updateProduct/:id", async (req, res) => {
-  var sql = `UPDATE Producto SET NombreProducto = '${req.body.name}', Descripcion = '${req.body.description}', PrecioUnitario = '${req.body.price}' WHERE IDProducto = ${req.params.id}`;
+  var sql = `UPDATE Producto SET NombreProducto = '${req.body.name}', Descripcion = '${req.body.description}', PrecioUnitario = '${req.body.price}', Habilitado = '${req.body.habilitado}' WHERE IDProducto = ${req.params.id}`;
 
   conn.query(sql, (err, result) => {
     if (err) console.error(err);
