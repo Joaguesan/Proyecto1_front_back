fd6d0e78afef8be6c9bb8147754a773e3dd70968
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2023-10-31 08:30:49 +0100

    changes to create order route

--- node/index.js
+++ node/index.js
@@ -2,57 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const cors = require("cors");
-var CryptoJS = require("crypto-js");
+const http = require("http");
 // const cookieParser = require("cook0ie-parser");
 var history = require("connect-history-api-fallback");
 
 const staticFieldMiddleware = express.static("public");
 
 const app = express();
-
-app.use(bodyParser.json());
-
-
-app.use(cors({
-  origin: "http://localhost:3001",
-  credentials: true
-}));
-
-const http = require('node:http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3001",
-    credentials: true
-  }
-});
-const port = 3000;
-
-server.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
-});
-
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  io.emit('connectat', "connectat");
-
-  socket.on('Acceptada', (id) => {
-    //Canviar estat a acceptat
-    var sql = `UPDATE Pedido SET Estado = "Acceptades" WHERE IDPedido = ${id}`;
-    conn.query(sql, (err, result) => {
-      if (err) console.error(err);
-      console.log(result);
-    });
-  })
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-});
+const io = new Server(server);
+app.use(bodyParser.json());
+app.use(cors());
 
 const sessionsSecret = "2hCTcL2p5QMSny6DbZtUFjVtVXZqFa";
 const sessionConfig = {
@@ -68,6 +29,7 @@ const sessionConfig = {
 // app.use(cookieParser({}));
 app.use(session(sessionConfig));
 
+const port = 3000;
 
 const mysql = require("mysql2");
 
@@ -92,6 +54,7 @@ conn.getConnection((err, connection) => {
 app.get("/", async (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
 io.on("connection", (socket) => {
   console.log("User connected");
   socket.on("disconnect", () => {
@@ -101,7 +64,6 @@ io.on("connection", (socket) => {
     io.emit("chat message", msg);
   });
 });
-
 
 app.get("/getProducts", async (req, res) => {
   var sql = "SELECT * FROM Producto";
@@ -187,7 +149,7 @@ FROM
   Producto
 JOIN
   DetallePedido ON Producto.IDProducto = DetallePedido.IDProducto
-JOIN
+JOIN 
   Pedido ON DetallePedido.IDPedido = Pedido.IDPedido
   WHERE Pedido.IDPedido = ${req.params.id}`;
 
@@ -203,7 +165,7 @@ JOIN
 });
 
 app.post("/createOrder", async (req, res) => {
-  var sql = `INSERT INTO Pedido (IDCliente,FechaPedido,Total,Estado,Comentario) VALUES ('${req.body.idClient}','${req.body.date}','${req.body.total}','Pendiente','${req.body.comentario}')`;
+  var sql = `INSERT INTO Pedido (IDCliente,Total,Comentario) VALUES ('${req.body.idClient}','${req.body.total}','${req.body.comentario}')`;
 
   conn.query(sql, (err, result) => {
     if (err) console.error(err);
@@ -239,8 +201,7 @@ app.post("/login", async (req, res) => {
 
     conn.query(sql, (err, result) => {
       if (err) console.error(err);
-      var ciphertext = CryptoJS.MD5(req.body.password).toString();
-      if (result == 0 || result[0].Contrasena != ciphertext) {
+      if (result == 0 || result[0].Contrasena != req.body.password) {
         res.status(500).send("Wrong email or password");
       } else {
         req.session.user = result[0].CorreoElectronico;
@@ -251,16 +212,15 @@ app.post("/login", async (req, res) => {
   }
 });
 
-
-
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
 });
 
 app.use(staticFieldMiddleware);
-app.use(history({
-  disableDotRules: true,
-  verbose: true,
-}));
+app.use(
+  history({
+    disableDotRules: true,
+    verbose: true,
+  })
+);
 app.use(staticFieldMiddleware);
