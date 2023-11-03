4a090e33155f21b5f988a5634c193cb3f2be3b91
IsmaelAlGh <a21ismalgelo@inspedralbes.cat>
2023-11-03 10:53:19 +0100

    fix errores a producció

--- node/index.js
+++ node/index.js
@@ -32,7 +32,7 @@ const io = new Server(server, {
     credentials: true,
   },
 });
-const port = 3000;
+const port = 3333;
 server.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
@@ -43,21 +43,42 @@ io.on("connection", (socket) => {
 
   socket.on("Acceptada", (id) => {
     //Canviar estat a acceptat
-    var sql = `UPDATE Pedido SET Estado = "Acceptades" WHERE IDPedido = ${id}`;
+    var sql = `UPDATE Pedido SET Estado = "En Preparacio" WHERE IDPedido = ${id}`;
     conn.query(sql, (err, result) => {
       if (err) console.error(err);
       console.log(result);
     });
+    io.emit("comandaNova");
   });
-
   socket.on("Rebutjada", (id) => {
-    var sql = `UPDATE Pedido SET Estado = "Rebutjada" WHERE IDPedido = ${id}`;
+    var sql = `UPDATE Pedido SET Estado = "Rebutjades" WHERE IDPedido = ${id}`;
 
     conn.query(sql, (err, result) => {
       if (err) console.error(err);
       console.log(result);
     });
+    io.emit("comandaNova");
   });
+  socket.on("Llesta", (id) => {
+    var sql = `UPDATE Pedido SET Estado = "Preparades" WHERE IDPedido = ${id}`;
+
+    conn.query(sql, (err, result) => {
+      if (err) console.error(err);
+      console.log(result);
+    });
+    io.emit("comandaNova");
+  });
+  socket.on("Entregada", (id) => {
+    var sql = `UPDATE Pedido SET Estado = "Entregades" WHERE IDPedido = ${id}`;
+
+    conn.query(sql, (err, result) => {
+      if (err) console.error(err);
+      console.log(result);
+    });
+    io.emit("comandaNova");
+  });
+
+  /*
 
   socket.on("Completada", (id) => {
     var sql = `UPDATE Pedido SET Estado = "Completada" WHERE IDPedido = ${id}`;
@@ -66,7 +87,7 @@ io.on("connection", (socket) => {
       console.log(result);
     });
   });
-
+*/
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
@@ -115,6 +136,7 @@ function descargarImagen(url, carpetaDestino, nombreArchivo) {
         );
         return;
       }
+
 
       const archivoDestino = `${carpetaDestino}/${nombreArchivo}`;
       const escrituraStream = fs.createWriteStream(archivoDestino);
@@ -188,7 +210,7 @@ app.get("/getOneProduct/:id", async (req, res) => {
 });
 
 app.post("/addProduct", async (req, res) => {
-  var imagen = "http://localhost:3000/imagen/" + req.body.Imatge + ".jpg";
+  var imagen = "http://damtr1g3.dam.inspedralbes.cat:3333/imagen/" + req.body.Imatge + ".jpg";
   var sql = `INSERT INTO Producto (NombreProducto,Descripcion,PrecioUnitario, Imatge) VALUES ('${req.body.name}','${req.body.description}','${req.body.price}','${imagen}')`;
 
   conn.query(sql, (err, result) => {
@@ -199,20 +221,16 @@ app.post("/addProduct", async (req, res) => {
 });
 app.delete("/deleteProduct/:id", async (req, res) => {
   var sql = `DELETE FROM Producto WHERE IDProducto = ${req.params.id}`;
-  conn.query(
-    `SELECT NombreProducto FROM Producto WHERE IDProducto = ${req.params.id}`,
-    function (err, result, fields) {
-      if (err) throw err;
-      try {
-        fs.unlinkSync(
-          "./assets/" + "producto" + result[0].NombreProducto + ".jpg"
-        );
-        console.log("Delete File successfully.");
-      } catch (error) {
-        console.log(error);
-      }
+  conn.query(`SELECT NombreProducto FROM Producto WHERE IDProducto = ${req.params.id}`, function (err, result, fields) {
+    if (err) throw err;
+    try {
+      fs.unlinkSync('./assets/' + "producto" + result[0].NombreProducto + '.jpg');
+      console.log("Delete File successfully.");
+    } catch (error) {
+      console.log(error);
     }
-  );
+  });
+
 
   conn.query(sql, (err, result) => {
     if (err) console.error(err);
@@ -222,7 +240,7 @@ app.delete("/deleteProduct/:id", async (req, res) => {
 });
 
 app.put("/updateProduct/:id", async (req, res) => {
-  var imagen = "http://localhost:3000/imagen/" + req.body.Imatge + ".jpg";
+  var imagen = "http://damtr1g3.dam.inspedralbes.cat:3333/imagen/" + req.body.Imatge + ".jpg";
   var sql = `UPDATE Producto SET NombreProducto = '${req.body.name}', Descripcion = '${req.body.description}', PrecioUnitario = '${req.body.price}', Imatge = '${imagen}' WHERE IDProducto = ${req.params.id}`;
 
   conn.query(sql, (err, result) => {
@@ -253,14 +271,14 @@ app.get("/getOrders", async (req, res) => {
 });
 
 app.get("/getOrdersClient/:id", async (req, res) => {
-  var sql = `SELECT * FROM Pedido WHERE IDCliente = ${req.params.id}`;
+  var sql = `SELECT * FROM Pedido WHERE IDCliente = '${req.params.id}'`;
 
   conn.query(sql, (err, result) => {
     if (err) console.error(err);
     console.log(result);
     res.send(result);
   });
-});
+})
 
 app.get("/detailOrder/:id", async (req, res) => {
   var sql = `SELECT
@@ -343,9 +361,9 @@ app.get("/renovarGrafico", (req, res) => {
   selectPedidos();
 });
 
-app.get("/mostrarGrafico", (req, res) => {
+app.get("/mostrarGraficoHoras", (req, res) => {
   selectPedidos();
-  mostrarGrafica();
+  mostrarGraficaHoras();
   res.sendFile(__dirname + "/estadisticas.jpeg");
 });
 
@@ -369,7 +387,7 @@ function selectPedidos() {
   });
 }
 
-function mostrarGrafica() {
+function mostrarGraficaHoras() {
   var { spawn } = require("child_process");
   var proceso = spawn("Python", ["./graficos.py"]);
 
@@ -394,6 +412,37 @@ function mostrarGrafica() {
     }
   });
 }
+app.get("/mostrarGraficoEstados",(req, res)=>{
+  selectPedidos();
+  mostrarGraficaEstado();
+  res.sendFile(__dirname + "/grafico2.jpeg");
+});
+
+function mostrarGraficaEstado(){
+  var { spawn } = require('child_process');
+  var proceso = spawn("Python",["./grafico2.py"]);
+  proceso.stdout.on("data",(data)=>{
+    console.log(`Salida estándar de Python: ${data}`);
+  });
+  proceso.on("close", (code) => {
+    if (code === 0) {
+      console.log("El script de Python se ha ejecutado correctamente.");
+    } else {
+      console.error(
+        `El script de Python ha finalizado con código de salida ${code}.`
+      );
+    }
+  });
+}
+
+app.get("/getClient/:id", async (req, res) => {
+  var sql = `SELECT Direccion, Telefono FROM Cliente WHERE IDCliente = ${req.params.id}`;
+  conn.query(sql, (err, result) => {
+    if (err) console.error(err);
+    console.log(result);
+    res.send(result);
+  });
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
